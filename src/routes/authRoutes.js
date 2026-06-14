261594085b5a96c3f5d4c74f93461c4787536ab2
voltakits <pertamake60gmail.com>
2026-06-15 05:42:59 +0700

    fix: udah ga error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+// Fungsi di-extract langsung di sini
 const { logout, loginGoogle } = require('../controllers/authController');
 
-router.post('/login-google', authController.loginGoogle);
+// Panggil langsung nama fungsinya!
+router.post('/login-google', loginGoogle);
 router.post('/logout', logout);
+
 module.exports = router;
