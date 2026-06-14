9fc8e19eb68f1d26c29804458a4e1fe64452305e
voltakits <pertamake60gmail.com>
2026-06-14 16:59:39 +0700

    fix: pasang app.options buat nangkep preflight CORS 404

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
 }));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser()); 
